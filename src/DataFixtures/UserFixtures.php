f0d4dbea1d9beb278b95a5b4a805f2883e1c2de7
= <=>
2023-01-02 19:10:09 +0100

    Mise en place de la sécurité et les comptes utilisateur

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    private array $roles;
+
+    /**
+     * @param UserPasswordHasherInterface $passwordHasher
+     */
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->roles = ['ROLE_USER', 'ROLE_RESTAURANT', 'ROLE_ADMIN'];
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        // Utilisation de faker
+        $faker = Factory::create("fr_FR");
+
+        for ($i=0; $i<20; $i++) {
+            $user = new User;
+            $user
+                ->setPrenom($faker->lastName)
+                ->setNom($faker->firstName)
+                ->setEmail($faker->email)
+                ->setCreatedAt(new \DateTime())
+                ->setActif($faker->boolean(90))
+                ->setRoles([$this->roles[$faker->numberBetween(0,2)]]);
+
+            if($faker->boolean(60)) $user->setPseudo($faker->userName);
+            
+            // AJOUTER MOT DE PASSE
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $faker->password
+            );
+            $user->setPassword($hashedPassword);
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
